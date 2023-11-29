9728d05c8a8e3613d911a7222aaa7d46b2c5179e
Iqbal Mahatma Putra <iqbal@alumni.pcr.ac.id>
2023-11-29 17:01:44 +0700

    oke

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#21594E',
+        secondary: '#64748b',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
